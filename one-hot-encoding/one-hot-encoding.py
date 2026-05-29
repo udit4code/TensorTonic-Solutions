61269117e8d595f1782c59e3ed3be7968c0bdd84
udit4code <udit.sharma820@gmail.com>
2026-05-29 20:54:20 +0530

    Update solution for one-hot-encoding

--- one-hot-encoding/one-hot-encoding.py
+++ one-hot-encoding/one-hot-encoding.py
@@ -9,10 +9,7 @@ def one_hot(y, num_classes=None):
         num_classes = np.max(y) + 1
     # Create all zeros
     result = np.zeros((len(y), num_classes), dtype=np.float32)
-
-    # Advanced indexing:
-    # rows:    [0,1,2,...N-1]
-    # columns: labels
+    # Advanced indexing: rows: [0,1,2,...N-1] and columns: labels
     # set those positions to 1
     result[np.arange(len(y)), y] = 1.0
     return result
