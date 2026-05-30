a4880225d9ac6f4eb34436f502488324ea071336
udit4code <udit.sharma820@gmail.com>
2026-05-30 14:15:42 +0530

    Update solution for numpy-reshape

--- study-plans/numpy-basics/numpy-reshape/numpy-reshape.py
+++ study-plans/numpy-basics/numpy-reshape/numpy-reshape.py
@@ -6,12 +6,16 @@ def reshape_array(data, operation):
     """
     np_data = np.array(data, dtype=np.float64)
     if operation == "flatten":
-         return np_data.flatten().copy()
+         np_data = np_data.flatten()
     elif operation == "transpose":
-        # Why .copy()? So that the returned object does not share the same memory location as np_data
-        return np_data.T.copy()
+        np_data = np_data.T
     elif operation == "add_batch":
         # Alternate way : np_data.reshape((1,) + np_data.shape)
         # It means : Via Tuple addition : (1, ) + (m, n) = (1, m, n)
-        return np.expand_dims(np_data, axis=0).copy()
-    raise Exception(f"operation {operation} is invalid.")
+        np_data = np.expand_dims(np_data, axis=0)
+    else:
+        raise Exception(f"operation {operation} is invalid.")
+    # Why .copy()? So that the returned object does not share the same memory location as np_data
+    result = np_data.copy()
+    assert id(result) != id(np_data) , f"np_data id : {id(np_data)} vs result id : {id(result)}"
+    return result
