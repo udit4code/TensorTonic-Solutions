cbbfd12d3d88187b5cd14f644d23bb466fea4fcd
udit4code <udit.sharma820@gmail.com>
2026-05-31 23:47:47 +0530

    Add solution for remove-stopwords

---
+++ remove-stopwords/remove-stopwords.py
@@ -0,0 +1,7 @@
+def remove_stopwords(tokens, stopwords):
+    """
+    Returns: list[str] - tokens with stopwords removed (preserve order)
+    """
+    # Your code here
+    stopwords = set(stopwords)
+    return [token for token in tokens if token not in stopwords]
