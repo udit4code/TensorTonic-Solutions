bf5d1de42f1b980f4039361b4fe889bd61501a20
udit4code <udit.sharma820@gmail.com>
2026-05-31 23:20:46 +0530

    Update solution for expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -11,4 +11,4 @@ def expected_value_discrete(x, p):
         raise ValueError("x and p must have the same shape")
     if abs(np.sum(p) - 1.0) > 1e-6:
         raise ValueError("probabilities must sum to 1")
-    return np.dot(x, p)
+    return np.dot(x, p) # Alternate : np.sum(x * p)
