e61f00f1aa5cb16f7a5827e647dd816a0150cc3a
udit4code <udit.sharma820@gmail.com>
2026-06-01 10:33:17 +0530

    Update solution for batch-generator

--- batch-generator/batch-generator.py
+++ batch-generator/batch-generator.py
@@ -4,7 +4,6 @@ def get_batch_util(X, y, batch_size, drop_last):
     n_samples = len(X)
     for start_idx in range(0, n_samples, batch_size):
         end_idx = start_idx + batch_size
-
         if end_idx > n_samples and drop_last:
             break
         yield (X[start_idx:end_idx], y[start_idx:end_idx])
@@ -19,12 +18,11 @@ def batch_generator(X, y, batch_size, rng=None, drop_last=False):
     y = np.array(y, dtype=np.float64)
 
     n_samples = len(X)
-
-    if rng is None:
-        rng = np.random.shuffle()
-
-    indices = rng.permutation(n_samples)
-
+    indices = np.arange(n_samples)
+    if rng:
+        rng.shuffle(indices)
+    else:
+        np.random.shuffle(indices)
     X_shuffled = X[indices]
     y_shuffled = y[indices]
 
