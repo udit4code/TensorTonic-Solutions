b8f356c29e85d942dce0815b1ca82292fdb248b7
udit4code <udit.sharma820@gmail.com>
2026-06-09 00:29:23 +0530

    Solve cosine-similarity

---
+++ cosine-similarity/cosine-similarity.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def cosine_similarity(a, b):
+    """
+    Compute cosine similarity between two 1D NumPy arrays.
+    Returns: float in [-1, 1]
+    """
+    # Write code here
+    a = np.array(a, dtype=np.float64)
+    b = np.array(b, dtype=np.float64)
+    a_dot_b = np.sum(a * b) 
+    l2_norm = lambda x : np.sqrt(np.sum(x * x))
+    norm_b = l2_norm(b)
+    norm_a = l2_norm(a)
+    if np.isclose(norm_a, 0.0) or np.isclose(norm_b, 0.0):
+        return 0.0
+    return a_dot_b / (norm_a * norm_b)
+    
