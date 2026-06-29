ef76db6bbadf27f9f7f7216e83680a610ec516bc
udit4code <udit.sharma820@gmail.com>
2026-06-30 01:07:12 +0530

    Solve rnn-cell

--- rnn/rnn-cell/rnn-cell.py
+++ rnn/rnn-cell/rnn-cell.py
@@ -1,11 +1,18 @@
 import numpy as np
 
 
+# This implementation can overflow for very large values of x. 
 def tanh(x):
     x = np.asarray(x)
     exp_pos = np.exp(x)
     exp_neg = np.exp(-x)
     return (exp_pos - exp_neg) / (exp_pos + exp_neg)
+
+# Numerically, more stable : 
+# tanh(x) = 2/(1 + exp(-2x)) - 1
+def tanh_v2(x): 
+    x = np.asarray(x)
+    return 2.0 / (1.0 + np.exp(-2.0 * x)) - 1.0
     
 def rnn_cell(x_t: np.ndarray, h_prev: np.ndarray, 
              W_xh: np.ndarray, W_hh: np.ndarray, b_h: np.ndarray) -> np.ndarray:
