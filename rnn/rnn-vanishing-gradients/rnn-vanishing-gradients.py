be9e14e785c227fe43596ec16edfd63cb1ef46b2
udit4code <udit.sharma820@gmail.com>
2026-06-30 15:00:35 +0530

    Solve rnn-vanishing-gradients

--- rnn/rnn-vanishing-gradients/rnn-vanishing-gradients.py
+++ rnn/rnn-vanishing-gradients/rnn-vanishing-gradients.py
@@ -10,11 +10,8 @@ def compute_gradient_norm_decay(T: int, W_hh: np.ndarray) -> list:
 
     spectral_norm = np.linalg.norm(W_hh, ord=2)
     norms = [1.0]
-
     grad_norm = 1.0
-
     for _ in range(1, T):
         grad_norm *= spectral_norm
         norms.append(float(grad_norm))
-
     return norms
