ddcb9cdc1229ba6c6e1d750c943ba9e0dadcfa2e
udit4code <udit.sharma820@gmail.com>
2026-07-04 19:41:42 +0530

    Solve pytorch-custom-dataclass

---
+++ study-plans/pytorch-basics/pytorch-custom-dataclass/pytorch-custom-dataclass.py
@@ -0,0 +1,20 @@
+import torch
+from torch.utils.data import Dataset
+
+class CSVDataset(Dataset):
+    """
+    Returns: (features, label) from __getitem__ where features is float32 (D,) and label is float32 (1,)
+    """
+
+    def __init__(self, data, label_col):
+        data_tensor = torch.tensor(data, dtype=torch.float32)
+        feature_cols = [idx for idx in range(data_tensor.shape[1]) if idx != label_col]
+        self.features = data_tensor[:, feature_cols]
+        self.labels = data_tensor[:, label_col].unsqueeze(1)
+        assert  len(self.features) == len(self.labels), "Mismatch in count of features and labels"
+        
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        return self.features[idx], self.labels[idx]
