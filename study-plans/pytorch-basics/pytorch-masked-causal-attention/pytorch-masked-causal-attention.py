69ced562b6a39e2dd9830a118eb79bd84be6455f
udit4code <udit.sharma820@gmail.com>
2026-07-04 22:23:30 +0530

    Solve pytorch-masked-causal-attention

--- study-plans/pytorch-basics/pytorch-masked-causal-attention/pytorch-masked-causal-attention.py
+++ study-plans/pytorch-basics/pytorch-masked-causal-attention/pytorch-masked-causal-attention.py
@@ -9,10 +9,7 @@ def causal_attention(Q, K, V):
     seq_q = scores.shape[-2]
     seq_k = scores.shape[-1]
 
-    # Boolean mask.
-    #
-    # True means:
-    # "Mask this position."
+    # Boolean mask. True means: "Mask this position."
     mask = torch.triu(
         torch.ones(
             seq_q,
@@ -22,10 +19,7 @@ def causal_attention(Q, K, V):
         ),
         diagonal=1
     )
-
     # Replace future-token attention scores with -inf.
     scores = scores.masked_fill(mask, float("-inf"))
-
     attention = torch.softmax(scores, dim=-1)
-
     return attention @ V
