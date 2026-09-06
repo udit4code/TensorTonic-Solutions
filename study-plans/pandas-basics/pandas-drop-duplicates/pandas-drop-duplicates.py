59aec06213d016d2564ec3d2d2f9d9fff5898cdf
udit4code <udit.sharma820@gmail.com>
2026-09-06 17:29:47 +0530

    Solve pandas-drop-duplicates

---
+++ study-plans/pandas-basics/pandas-drop-duplicates/pandas-drop-duplicates.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def drop_duplicates(data):
+    """
+    Returns: list [rows_before, rows_after, cleaned_data]
+    """
+    # Step 1 : Create DataFrame
+    df = pd.DataFrame(data)
+    rows_before = len(df)
+    # Step 2 : Drop duplicates from df. 
+    # The drop_duplicates() method checks all columns by default and removes rows where every value matches a previously seen row. 
+    # It keeps the first occurrence by default.
+    df = df.drop_duplicates()
+    rows_after = len(df)
+    
+    return [
+        rows_before, 
+        rows_after, 
+        df.to_dict("list")
+    ]
