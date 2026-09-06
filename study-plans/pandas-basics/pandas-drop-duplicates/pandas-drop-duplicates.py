71441137a193f2dfc95724ff826e8b932d72be86
udit4code <udit.sharma820@gmail.com>
2026-09-06 17:32:41 +0530

    Solve pandas-drop-duplicates

--- study-plans/pandas-basics/pandas-drop-duplicates/pandas-drop-duplicates.py
+++ study-plans/pandas-basics/pandas-drop-duplicates/pandas-drop-duplicates.py
@@ -10,8 +10,14 @@ def drop_duplicates(data):
     # Step 2 : Drop duplicates from df. 
     # The drop_duplicates() method checks all columns by default and removes rows where every value matches a previously seen row. 
     # It keeps the first occurrence by default.
+    # Collect the number of duplicate rows
+    # The duplicated() method returns a boolean Series marking duplicate rows. 
+    # By default, keep=first, which means that the first occurrence is not a duplicate, while the subequent ones are. 
+    duplicate_row_count = df.duplicated().sum()
     df = df.drop_duplicates(keep="first")
     rows_after = len(df)
+
+    assert rows_before - rows_after == duplicate_row_count
     
     return [
         rows_before, 
