e29db2950453acd4601178d421c2c5d6ebc8217a
udit4code <udit.sharma820@gmail.com>
2026-09-06 16:57:04 +0530

    Solve pandas-multi-column-selection

--- study-plans/pandas-basics/pandas-multi-column-selection/pandas-multi-column-selection.py
+++ study-plans/pandas-basics/pandas-multi-column-selection/pandas-multi-column-selection.py
@@ -5,5 +5,7 @@ def select_columns(data, columns):
     Returns: dict mapping selected column names to value lists
     """
     df = pd.DataFrame(data)
+    # The most direct way to select multiple columns is to pass a list of column names inside brackets: subset = df[['name', 'age', 'salary']] 
+    # This always returns a DataFrame, even if the list contains only one name. The columns appear in the order specified in the list.
     selected_sub_df = df[columns]
     return selected_sub_df.to_dict("list")
