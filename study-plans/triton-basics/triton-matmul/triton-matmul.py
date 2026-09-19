b39924702e7684ddc9ffd59dbfdb2961530fc9a9
udit4code <udit.sharma820@gmail.com>
2026-09-19 16:37:13 +0530

    Solve triton-matmul

--- study-plans/triton-basics/triton-matmul/triton-matmul.py
+++ study-plans/triton-basics/triton-matmul/triton-matmul.py
@@ -18,6 +18,20 @@ def matmul_kernel(
     # Step 1 : GRID IDENTIFICATION
     # Each Triton program instance computes one [BLOCK_M x BLOCK_N] tile of the output matrix C.
     # We find out WHICH tile this specific program is computing using program IDs.
+    # DOUBT : Why are we calling tl.program_id twice ? 
+    # tl.program_id(axis) returns a single integer, not a tuple. 
+    # It returns the coordinate of the current GPU program instance along the specified dimension (axis) of our execution grid.
+    # The mental model is : when we launch matmul_kernel[(grid_m, grid_n)], Triton spins up grid_m * grid_n independent program instances. Each program instance is assigned to compute exactly one cell of that grid.
+    # We have to call tl.program_id(axis) twice, because, we are fetching coordinates, not different IDs.
+    # In other words, we are not getting two different program IDs; 
+    # we are getting the X and Y coordinates of the same program ID. 
+    # Because we launched a 2D grid, every program instance exists on a 2D plane. 
+    # To know exactly which chunk of the matrix it needs to process, the program needs to know its row and its column.
+    # If we were writing a simple kernel to add two 1D vectors together, we would launch a 1D grid: grid = (num_blocks,). 
+    # In that kernel, we would only call tl.program_id(0) because a 1D line only has one axis.
+    # Note for CUDA backgrounds: tl.program_id(0) is exactly equivalent to blockIdx.x in CUDA C++, and tl.program_id(1) is blockIdx.y
+    # Because matrix multiplication produces a 2D output matrix, it is mathematically much easier to map it to a 2D execution grid, which requires checking both axis 0 and axis 1.
+    
     pid_m = tl.program_id(0) # Row index of the tile
     pid_n = tl.program_id(1) # Column index of the tile
 
